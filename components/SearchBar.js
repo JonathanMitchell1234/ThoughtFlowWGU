2172a0665c61c7c59a79f87570d4dddaecbc4dcf
Jonathan Mitchell <jonmitchell1234@gmail.com>
2024-08-30 20:35:29 -0400

    fixed search bar styles

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,25 +1,8 @@
 import React from "react";
-import { TextInput, StyleSheet, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 const SearchBar = ({ onChangeText, value }) => {
-	return (
-		<View style={styles.container}>
-			<TextInput style={styles.input} placeholder="Search..." onChangeText={onChangeText} value={value} />
-		</View>
-	);
+	return <Searchbar placeholder="Search..." onChangeText={onChangeText} value={value} />;
 };
-
-const styles = StyleSheet.create({
-	container: {
-		// padding: 10,
-	},
-	input: {
-		height: 40,
-		borderColor: "#ccc",
-		borderWidth: 1,
-		borderRadius: 12,
-		paddingLeft: 10,
-	},
-});
 
 export default SearchBar;
